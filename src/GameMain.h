5c253a02daf8d65cc2d5bfa874fd2f9da60eb53b
みっちゃん <sansenjunye@gmail.com>
2023-12-01 21:45:44 +0900

    シーンの追加

--- src/GameMain.h
+++ src/GameMain.h
@@ -16,19 +16,5 @@ void GameDraw();
 // 終了処理
 void GameExit();
 
-// 移動時のお互いのあたり判定v
-void Move_Hit();
-
-// タイマーの更新処理
-void Time_Update();
-// タイマーの描画
-void Time_Draw();
 
 
-
-// 攻撃のあたり判定を行う関数
-void Attack_Hit(int player1, int player2);
-// ガードの当たり判定
-void Block_Hit(int player1, int player2);
-// キャラクターのステータス描画処理
-void Draw_Status();
