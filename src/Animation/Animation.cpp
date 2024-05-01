fc2b433dc2e547b3bc57a8dfe45318fe1461e081
みっちゃん <sansenjunye@gmail.com>
2024-05-01 23:18:00 +0900

    アニメーションクラスのアップグレード

--- src/Animation/Animation.cpp
+++ src/Animation/Animation.cpp
@@ -20,9 +20,25 @@ Animation::~Animation()
 	delete m_contexts;
 }
 
-
-void Animation::Init_Animation(int anim_max)
+// アニメーションの情報の初期化関数
+void Animation::Information_Init(int num)
 {
+	//アニメーションの情報をすべて初期化しておく
+	m_contexts[num].is_playering = false;
+	m_contexts[num].is_loop = false;
+	m_contexts[num].animation_index = -1;
+	m_contexts[num].animation_attach_index = -1;
+	m_contexts[num].m_blend_ratio = 0.0f;
+	m_contexts[num].animation_total_time = 0.0;
+	m_contexts[num].play_time = 0.0f;
+}
+
+// アニメーションの初期設定
+void Animation::Init_Animation(int anim_max, int default_anim)
+{
+	// デフォルトアニメーションの設定
+	m_default_anim = default_anim;
+	// アニメーションの入れ物を配列で確保する
 	for (int i = 0; i < anim_max; i++)
 	{
 		AnimData* anim = new AnimData;
@@ -43,6 +59,9 @@ void Animation::Load_Animation(const char file_path[256], int anim_no, int anim_
 // アニメーションのセット
 void Animation::Attach_Animation(Model* model, int anim_num, bool loop)
 {
+	// 入れ物を初期化しておく
+	Information_Init(0);
+
 	// アニメションのセット
 	m_contexts[0].animation_attach_index = MV1AttachAnim
 	(
@@ -69,7 +88,6 @@ void Animation::Attach_Animation(Model* model, int anim_num, bool loop)
 
 	// ループするかどうか
 	m_contexts[0].is_playering = loop;
-
 }
 
 // アニメーションの取り外し
@@ -80,28 +98,93 @@ void Animation::Detach_Animation(Model* model)
 	(model->m_model,
 		m_contexts[1].animation_attach_index);
 	// 取り外したアニメーションの情報をすべて初期化しておく
-	m_contexts[1].is_playering = false;
-	m_contexts[1].is_loop = false;
-	m_contexts[1].animation_index = -1;
-	m_contexts[1].animation_attach_index = -1;
-	m_contexts[1].m_blend_ratio = 0.0f;
-	m_contexts[1].animation_total_time = 0.0;
-	m_contexts[1].play_time = 0.0f;
-	
+	Information_Init(1);
 }
 
 
 // アニメーション切り替え用関数
-void Animation::Change_Animation(Model* model, int anim_num)
+void Animation::Change_Animation(Model* model, int anim_num , bool loop)
 {
+	// アニメーションを入れ替える前にもともとついていアニメーションを取り外す
+	Detach_Animation(model);
 	// 中にはいている情報を入れ替える
 	m_contexts[1] = m_contexts[0];
+
+	// 次に行いたいアニメーションをつける
+	Attach_Animation(model, anim_num, loop);
+
+	// 次のアニメーションのブレンド率を上げる
+	m_contexts[0].m_blend_ratio += 0.1f;
+	// 上限の設定
+	if (m_contexts[0].m_blend_ratio > 1.0f)
+	{
+		m_contexts[0].m_blend_ratio = 1.0f;
+	}
+	// もともとのアニメーションのブレンド率を下げる
+	m_contexts[1].m_blend_ratio -= 0.1f;
+	// 下限の設定
+	if (m_contexts[1].m_blend_ratio < 0)
+	{
+		m_contexts[1].m_blend_ratio = 0.0f;
+	}
 }
 
+// アニメーション切り替え用関数（アニメーションが終わるタイミングで切り替える）
+void Animation::Action_Change_Animation(Model* model, int anim_num, bool loop)
+{
+	// アニメーションが最後のフレームに差し掛かったら
+	if (m_contexts[0].animation_total_time >= m_contexts[0].play_time)
+	{
+		Change_Animation(model, anim_num, loop);
+	}
+}
 
 // アニメーションを再生させるための関数
-void Animation::Play_Animation()
+void Animation::Play_Animation(Model* model)
 {
+
+	m_contexts[0].play_time++;
+	// ループするかどうかによって処理の変更
+	if (m_contexts[0].is_loop)
+	{
+		Loop_Animation();
+	}
+	else // ループしない場合
+	{
+		Not_Loop(model);
+	}
+
+	// それぞれにアニメーションの割合分再生します
+	for (int i = 0; i < 2; i++)
+	{
+		MV1SetAttachAnimBlendRate(
+			model->m_model,
+			m_contexts[i].animation_attach_index,
+			m_contexts[i].play_time);
+	}
+}
+
+// アニメーションをループさせる
+void Animation::Loop_Animation()
+{
+
+	// アニメーションフレームが最後まで回ったら
+	if (m_contexts[0].play_time > m_contexts[0].animation_total_time)
+	{
+		// 最初にリセットする
+		m_contexts[0].play_time = 0.0f;
+	}
+
+}
+
+void Animation::Not_Loop(Model* model)
+{
+	// アニメーションフレームが最後まで回ったら
+	if (m_contexts[0].play_time >= m_contexts[0].animation_total_time)
+	{
+		// デフォルトのアニメーションに設定
+		Change_Animation(model, m_default_anim, true);
+	}
 
 }
 
--- src/Animation/Animation.h
+++ src/Animation/Animation.h
@@ -11,15 +11,16 @@
 class Animation
 {
 public:
-
-	//! コンストラクタ
-	//! @param アニメーションの最大数
+	
+	//!@brief コンストラクタ
+	//!@param アニメーションの最大数
 	Animation();
 
-
-	//! デストラクタ
+	
+	//!@brief デストラクタ
 	virtual ~Animation();
 
+	//! アニメーションのデータ用の構造体
 	struct AnimData
 	{
 		int handle = -1;     //! アニメーションの入れ物
@@ -27,45 +28,8 @@ public:
 	};
 	std::vector<AnimData*> anim_handle;
 
-
-	void Init_Animation(int anim_max);
-
-	//! 読み込み用関数
-	//! @param ファイルのパス
-	//! @param アニメーション番号
-	void Load_Animation(const char file_path[256], int anim_no, int anim_index);
-
-
-
-	//! アニメーションをセットする関数
-	//! @param ベースのモデル
-	//! @param アッタチしたいモデル番号
-	//! @param ループ再生させるかどうか
-	void Attach_Animation(Model* model, int anim_num, bool loop);
-
-
-	//! ついているアニメーションを取り外すようの関数
-	//! @param ベースのモデル
-	//! @param デタッチしたいモデル番号
-	void Detach_Animation(Model* model);
-
-
-	//! アニメーションの切り替え用関数
-	//! @param ベースのモデル
-	//! @param 次にセットしたいアニメーション番号
-	void Change_Animation(Model* model, int anim_num);
-
-	//! アニメーションの再生
-	void Play_Animation();
-
-private:
-
-	int          m_anim_max = 0;          //!< アニメーションの最大数
-public:
-
-
 	//! 再生中のアニメーションの情報構造体
-	struct Context
+	struct Information
 	{
 		bool  is_playering = false;         //!< 再生中かどうか
 		bool  is_loop = false;              //!< ループ再生かどうか
@@ -75,53 +39,71 @@ public:
 		float animation_total_time = 0.0;   //!< 総再生時間
 		float play_time = 0.0f;             //!< 現在再生中の時間
 	};
-
+	//! 構造体はアニメーションブレンドのため2系統を持つ
 	//! [0]現在のアニメーション
 	//! [1]前のアニメーション
-	Context m_contexts[2];           //!< 構造体はアニメーションブレンドのため2系統を持つ
+	Information m_contexts[2];          
+
+	//! @brief アニメーションの情報の初期化関数
+	//! @param どちらの構造体を初期化するかの番号
+	void Information_Init(int num);
+
+	//! @brief アニメーションの初期設定
+	//! @param アニメーションの数
+	//! @param デフォルト設定したいアニメーションの番号
+	//! @detail デフォルトアニメーションのセット、アニメーションの入れ物の確保
+	void Init_Animation(int anim_max, int default_anim);
+
+	
+	//! @brief読み込み用関数
+	//! @param ファイルのパス
+	//! @param アニメーション番号
+	void Load_Animation(const char file_path[256], int anim_no, int anim_index);
 
 
-	float   m_blend_time = 1.0f;    //!< ブレンドの補間時間
+	//! @briefアニメーションをセットする関数
+	//! @param ベースのモデル
+	//! @param アッタチしたいモデル番号
+	//! @param ループ再生させるかどうか
+	void Attach_Animation(Model* model, int anim_num, bool loop);
 
 
-	////---------------------------------------------------------------------------
-	//// 普通アニメーション読み込み用の配列
-	////---------------------------------------------------------------------------
-	//
-	//int*   m_anim_model = 0;    // アニメーションモデル
-	//int*   m_anim_attach = 0;   // モデルにアニメーションをアタッチする用の配列（アタッチ番号を取得する）
-	//float* m_anim_total = 0;  // アニメーションが何フレーム科を取得、それを保存する用の配列
-	//float* m_anim_rate = 0;	// それぞれのアニメーションのブレンド率（0.0f ~ 1.0f）
-	//float* m_anim_frame = 0;  // アニメーションが何フレーム進んでいるのか
+	//! @briefついているアニメーションを取り外すようの関数
+	//! @param ベースのモデル
+	//! @param デタッチしたいモデル番号
+	void Detach_Animation(Model* model);
 
 
-	////---------------------------------------------------------------------------
-	//// 攻撃アニメーション用変数
-	////---------------------------------------------------------------------------
-	//int* m_attack_anim_model = 0;    // 各攻撃アニメーションアニメーション読み込み用の配列
-	//int* m_attack_anim_attach = 0;   // モデルに攻撃アニメーションをアタッチする用の配列（アタッチ番号を取得する）
-	//float* m_attack_anim_total = 0;  // 攻撃アニメーションが何フレーム科を取得、それを保存する用の配列
-	//float* m_attack_anim_rate = 0;   // それぞれの攻撃アニメーションのブレンド率（0.0f ~ 1.0f）
-	//float* m_attack_anim_frame = 0;  // 攻撃アニメーションが何フレーム進んでいるのか
+	//! @briefアニメーションの切り替え用関数
+	//! @param ベースのモデル
+	//! @param 次にセットしたいアニメーション番号
+	//! @param アニメーションをループさせるかどうか
+	void Change_Animation(Model* model, int anim_num, bool loop);
+
+	//! @brief アニメーションの切り替え用関数
+	//! @param ベースのモデル
+	//! @param 次にセットしたいアニメーション番号
+	//! @param アニメーションをループさせるかどう
+	//! @detail アニメーションが終わるタイミングで切り替えるでChange_Animationを呼びたい時用の関数
+	void Action_Change_Animation(Model* model, int anim_num, bool loop);
+
+	//! @briefアニメーションの再生
+	//! @param ベースのモデル
+	void Play_Animation(Model* model);
 
 
-	////---------------------------------------------------------------------------
-	//// ガードアニメーション用変数
-	////---------------------------------------------------------------------------
-	//int* m_block_anim_model = 0;         // アニメーションモデル
-	//int* m_block_anim_attach = 0;        // モデルにアニメーションをアタッチする用の配列（アタッチ番号を取得する）
-	//float* m_block_anim_total = 0;       // アニメーションが何フレーム科を取得、それを保存する用の配列
-	//float* m_block_anim_rate = 0;	       // それぞれのアニメーションのブレンド率（0.0f ~ 1.0f）
-	//float* m_block_anim_frame = 0;       // アニメーションが何フレーム進んでいるのか
+	//! @briefアニメーションをループさせる
+	void Loop_Animation();
+
+	//! @briefループさせない場合
+    //! @param ベースのモデル
+	void Not_Loop(Model* model);
 
 
-	////---------------------------------------------------------------------------
-	//// ダメージ（受けた時の）アニメーション用変数
-	////---------------------------------------------------------------------------
-	//int* m_damage_anim_model = 0;    // アニメーションモデル
-	//int* m_damage_anim_attach = 0;   // モデルにアニメーションをアタッチする用の配列（アタッチ番号を取得する）
-	//float* m_damage_anim_total = 0;  // アニメーションが何フレーム科を取得、それを保存する用の配列
-	//float* m_damage_anim_rate = 0;   // それぞれのアニメーションがどのくらうのわりあいになるか（0.0f ~ 1.0f）
-	//float* m_damage_anim_frame = 0;  // アニメーションが何フレーム進んでいるのか
+
+private:
+
+	int m_anim_max = 0;          //!< アニメーションの最大数
+	int m_default_anim = -1;     //!< デフォルトでセットしたアニメーション番号用の変数
 
 };
